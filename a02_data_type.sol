61147f0430712ec158ece6fe69915c2842b8909f
mistertandon <enggparveshtandon@gmail.com>
2022-02-05 18:00:28 +0530

    feat: Data type in Solidity.

---
+++ a02_data_type.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.7;
+
+contract ValueTypes{
+
+    uint256 public uint256Min = type(uint256).min;
+
+    uint256 public uint256Max = type(uint256).max;
+
+    address public randomEthAddr = 0xdCad3a6d3569DF655070DEd06cb7A1b2Ccd1D3AF;
+}
