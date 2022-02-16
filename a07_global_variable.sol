1b8fd5c1bba6c72143c409dcf39bdcd24b560309
mistertandon <enggparveshtandon@gmail.com>
2022-02-16 22:06:01 +0530

    feat: Solidity global valraible.

---
+++ a07_global_variable.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract GlobalVariable{
+
+    function globalVars() external view returns (address, uint256, uint256) {
+
+        address sender = msg.sender;
+        uint256 blockNum = block.number;
+        uint256 blcokTimestamp = block.timestamp;
+
+        return (sender, blockNum, blcokTimestamp);
+    }
+}
