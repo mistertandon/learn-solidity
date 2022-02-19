f99825033a5164e3d2a4087ac709fbeac7e70a1c
mistertandon <enggparveshtandon@gmail.com>
2022-02-19 23:19:32 +0530

    feat: Delete element from array solidity.

---
+++ a20_delete_by_shifting.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract ArrayShift{
+
+    uint256[] public numArray;
+
+    function getNumArray() public view returns (uint256[] memory){
+        return numArray;
+    }
+
+    function addElement(uint256 _element) public {
+        numArray.push(_element);
+    }
+
+    modifier validateNumArrayLength(uint256 _idx) {
+        require(_idx < numArray.length, "Array overflow");
+        _;
+    }
+
+    function deleteElementAtIndex(uint256 _idx) public validateNumArrayLength(_idx){
+        
+        for(uint256 i = _idx; i< numArray.length -1; i++){
+            numArray[i] = numArray[i+1];
+        }
+
+        numArray.pop();
+    }
+}
