d6caa8e05b0196cf7741a88a10a1280c97e78f86
mistertandon <enggparveshtandon@gmail.com>
2022-03-02 23:42:10 +0530

    feat: Pure function in solidity.

---
+++ a08_function_view_pure.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract PureAndViewFunctions {
+
+    uint256 stateVNum;
+
+    function increaseStateVNum() external {
+        stateVNum +=1;
+    }
+
+    function getStateVNum() external view returns (uint256){
+        return stateVNum;
+    }
+
+    function pureAndViewFunc(uint256 x) external view returns (uint256){
+        return stateVNum + x;
+    }
+
+    function pureFunc(uint256 x, uint256 y) external pure returns (uint256){
+        return x + y;
+    }
+}
