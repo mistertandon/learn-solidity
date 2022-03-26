e3cf21290dc7169ca511416d8ffea1710334a8e9
mistertandon <enggparveshtandon@gmail.com>
2022-03-26 23:50:38 +0530

    feat: Event in solidity.

---
+++ a300_event.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract Event{
+
+    event Log(string message, uint256 val);
+
+    event IndexedLog(address indexed sender, uint256 val);
+
+    function foo() external {
+        emit Log("Solidity event", 18100000);
+    }
+
+    function bar() external {
+        emit IndexedLog(msg.sender, 19100000);
+    }
+}
