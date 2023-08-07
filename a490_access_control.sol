1711d64f9d3a062958ef9918ab6ca0fbc48c8737
mistertandon <enggparveshtandon@gmail.com>
2023-08-07 17:30:48 +0530

    fix: Rename file name to compliance naming convention

--- a490_access_control.sol
+++ a490_access_control.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.3;
 contract AccessControl {
     event GrantRole(bytes32 indexed role, address indexed account);
     event RevokeRole(bytes32 indexed role, address indexed account);
